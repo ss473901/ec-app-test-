1b9b0bea8dc4a1af9871bbe7c00caebe23e02dcb
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-04-06 17:57:51 +0900

    add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,60 @@ import { Register } from "./pages/Register";
 import { Cart } from "./pages/Cart";
 import { Login } from "./pages/Login";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:category" element={<ProductList />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+
+        {/* <Route path="/login" element={<Login />}>
+          {user ? <Navigate to="/" /> : <Login />}
+        </Route> */}
+
+        {/* <Route path="login" element={<Login />}>
+          {user ? (
+            <Route path="/" element={<Home />} />
+          ) : (
+            <Route path="/login" element={<Login />} />
+          )}
+        </Route> */}
+
+        {/* <Route>
+          {user ? (
+            <Route path="/" element={<Home />} />
+          ) : (
+            <Route path="/login element={<Login/>}" />
+          )}
+        </Route> */}
+
+        {/* 
+        {user ? (
+          <Route path="/" element={<Home />} />
+        ) : (
+          <Route path="/login" element={<Login />} />
+        )} */}
+
+        {/* {user ? (
+          <Route>
+            <Navigate to="/" />
+          </Route>
+        ) : (
+          <Route path="/login" element={<Login />} />
+        )} */}
+
+        {user ? (
+          <Navigate to="/" />
+        ) : (
+          <Route path="/login" element={<Login />} />
+        )}
+        
       </Routes>
     </BrowserRouter>
   );
