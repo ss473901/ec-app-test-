b0e10969738eea3f56b8377ec6e3639a4b9e3ec1
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-04-15 00:52:02 +0900

    Add login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { Success } from "./pages/Success";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  const user = true;
+  const user = false;
   return (
     <BrowserRouter>
       <Routes>
--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,14 +1,36 @@
 import * as Styled from "./style";
+import { useState } from "react";
+import { login } from "../../redux/apiCalls";
+import { useDispatch, useSelector } from "react-redux";
 
 export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
   return (
     <Styled.Container>
       <Styled.Wrapper>
         <Styled.Title>SIGN IN</Styled.Title>
         <Styled.Form>
-          <Styled.Input placeholder="username" />
-          <Styled.Input placeholder="password" />
-          <Styled.Button>LOGIN</Styled.Button>
+          <Styled.Input
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Styled.Input
+            placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Styled.Button onClick={handleClick} disable={isFetching}>
+            LOGIN
+          </Styled.Button>
+          {error && <Styled.Error>問題が発生しました</Styled.Error>}
           <Styled.Link>DO NOT YOU REMEMBER THE PASSWORD?</Styled.Link>
           <Styled.Link>CREATE A NEW ACCOUNT</Styled.Link>
         </Styled.Form>
