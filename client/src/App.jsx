a64268c52bcf9aeca3c6993476b3464cee7929fb
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-04-26 22:40:44 +0900

    Add Navigate register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,10 @@ function App() {
         <Route path="/product/:id" element={<Product />} />
         <Route path="/success" element={<Success />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
       </Routes>
     </BrowserRouter>
